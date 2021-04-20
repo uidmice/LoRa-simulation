e917474a953679655eea1ea16262a392efe2473a
uidmice <438148558@qq.com>
2021-04-19 21:17:27 -0400

    output grid temperature data

--- predict.py
+++ predict.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 from Simulation import Simulation
-from framework.utils import load_config, random_policy, field_construct_data
+from framework.utils import load_config, random_policy, field_construct_data, random_network_construct, play_field_video
 from config import *
 import pickle
+from framework.Gateway import  Gateway
 
 DEBUG = False
 
@@ -12,26 +13,33 @@ num_steps = 1000
 step_time = 6000  # ms
 offset = 3000
 fire_update = 18000
-config = 'config2'
+config = 'random1'
 
-gateway_location, node_locations, connection = load_config(config)
-simulation = Simulation(node_locations, gateway_location, step_time, connection, config, offset=offset, update_rate=fire_update)
+gateway_location, node_locations, connection, distance = load_config(config)
+simulation = Simulation(node_locations, gateway_location, step_time, connection, config, distance, num_steps, offset=offset, update_rate=fire_update)
+
+# dT = np.diff(simulation.environment.T_field, axis=0)
+# print(np.max(dT))
+#
 # policy = lambda x: random_policy(0.5, x)
-# policy.name = "random_0.1"
+# policy.name = "random_0.5"
 # field_construct_data(simulation, num_steps, step_time/1000, policy, scale=100, show=True)
+# random_network_construct(simulation, num_steps, policy)
 
- # STATE_KEYWORDS = ["location", "failure_rate", "last_update", "current_sensing",
-    #                   "num_unique_packets_received", "num_total_packets_sent", "total_transmit_time",
-    #                   "total_receive_time", "total_energy_usage", "last_packet_success"]
 
-repeat = 5
-random_para = [0.1,0.2,0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+repeat = 3
+random_para = [0.1,0.2, 0.3, 0.4,0.5,0.6,0.7,0.8,0.9,1]
 PER_random = np.zeros((len(random_para), repeat, num_steps))
 pos_reward_random = np.zeros((len(random_para), repeat, num_steps, len(node_locations)))
 success_nodes = np.zeros((len(random_para), repeat, num_steps, len(node_locations)))
 send_nodes = np.zeros((len(random_para), repeat, num_steps, len(node_locations)))
 real_field = np.zeros((len(random_para), repeat, num_steps, len(node_locations)))
 constructed_field = np.zeros((len(random_para), repeat, num_steps, len(node_locations)))
+
+temp_field = np.zeros((num_steps, 2* distance//GRID + 1, 2* distance//GRID + 1))
+
+for i in range(num_steps):
+    temp_field[i, :, :] = simulation.environment.T_field[i*step_time//fire_update]
 
 for j, prob in enumerate(random_para):
     print("Probability: "+ str(prob))
@@ -55,7 +63,9 @@ for j, prob in enumerate(random_para):
             pos_reward_random[j,i,k,:] = pos_reward * success_array
             n = max(len(send), 1)
             PER_random[j, i, k] = len(success) / n
-name = "result/"+ simulation.name +"_random_full.pickle"
-pickle.dump([PER_random, pos_reward_random, send_nodes, success_nodes, real_field, constructed_field], open(name, "wb"))
+
+name = "result/"+ config +"/no_channel_" + str(Gateway.NO_CHANNELS) +"/offset_"+str(offset) +"/update_" + str(fire_update) +"_random_full.pickle"
+
+pickle.dump([PER_random, pos_reward_random, send_nodes, success_nodes, real_field, constructed_field, temp_field], open(name, "wb"))
 
 
